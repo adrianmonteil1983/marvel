368c9fec219c1d3c6f40c6fba5ac4612a1130e69
adrianmonteil1983 <monteiladrian@gmail.com>
2020-06-11 17:41:44 -0400

    modif marvel keys

--- marvel_key.js
+++ marvel_key.js
@@ -1,2 +1,2 @@
-export const public_key = process_env_PUBLIC_KEY
-export const private_key= process_env_PRIVATE_KEY
+export const public_key = process.env.PUBLIC_KEY
+export const private_key= process.env.PRIVATE_KEY
