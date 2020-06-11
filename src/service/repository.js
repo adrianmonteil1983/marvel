094db8420deb568a57e744475513c63756ff41f2
adrianmonteil1983 <monteiladrian@gmail.com>
2020-06-11 17:44:47 -0400

    modif http to https request

--- src/service/repository.js
+++ src/service/repository.js
@@ -3,5 +3,5 @@ import {public_key} from '../../marvel_key.js';
 
 
 export const fetchCharacters = async () => {
-  return await axios.get(`http://gateway.marvel.com/v1/public/characters?apikey=${public_key}`)
+  return await axios.get(`https://gateway.marvel.com/v1/public/characters?apikey=${public_key}`)
 }
