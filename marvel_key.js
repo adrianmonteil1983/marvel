196692ea8faf8bcea82f40d9fabbcd19174f2812
adrianmonteil1983 <monteiladrian@gmail.com>
2020-06-11 20:59:20 -0400

    remove VUE APP to test

--- marvel_key.js
+++ marvel_key.js
@@ -1,2 +1,2 @@
-export const public_key = process.env.VUE_APP_PUBLIC_KEY
-export const private_key= process.env.VUE_APP_PRIVATE_KEY
+export const public_key = process.env.PUBLIC_KEY
+export const private_key= process.env.PRIVATE_KEY
